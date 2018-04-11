9d97e895037d85de002db18b15f7a1a96ef207c9
Kerby <kerby.geffrard@gmail.com>
2018-04-10 21:05:33 -0400

    Add cookie

--- main.cpp
+++ main.cpp
@@ -6,23 +6,41 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    // Basic stuffs
     QApplication app(argc, argv);
-    QWebEngineProfile::defaultProfile()->setPersistentCookiesPolicy(QWebEngineProfile::ForcePersistentCookies);
     QWebEngineView view;
+    QNetworkCookieJar cookieJar;
 
+    // Get the cookie store reference from the defaultProfile
     auto * profile = QWebEngineProfile::defaultProfile();
-    auto * store = profile->cookieStore();
+    auto * store = profile->cookieStore();  /** NOT EXPOSED TO PYSIDE2 **/
 
-    QObject::connect(store, &QWebEngineCookieStore::cookieAdded, [&](const QNetworkCookie &cookie) {
-        qInfo() << "Domain:\t" << cookie.domain() << "\n"
-                 << "Name:\t"   << cookie.name() << "\n"
-                 << "Value:\t"  << cookie.value() << "\n"
-                 << "Due:\t" << cookie.expirationDate() << "\n";
+    // Forge a cookie
+    QNetworkCookie cookie;
+    cookie.setDomain(argv[1]);
+    cookie.setName("My special cookie");
+    cookie.setValue("SessionId -> !@#!@#!@#!@#!#");
+
+    // Connect to the store to be able to keep the cookies
+    QObject::connect(store, &QWebEngineCookieStore::cookieAdded, [&](const QNetworkCookie &cookie) { /** NOT EXPOSED TO PYSIDE2 **/
+        cookieJar.insertCookie(cookie);
     });
 
+    // Set my newly forged cookie
+    store->setCookie(cookie, QUrl(argv[1])); /** NOT EXPOSED TO PYSIDE2 **/
+
+    // Show my page
     view.setUrl(QUrl(argv[1]));
     view.show();
+    app.exec();
 
-    return app.exec();
+    // Get every cookie related to my page.
+    for (auto cookie : cookieJar.cookiesForUrl(QUrl(argv[1]))){
+        qInfo() << "Domain:\t" << cookie.domain() << "\n"
+                << "Name:\t"   << cookie.name() << "\n"
+                << "Value:\t"  << cookie.value() << "\n"
+                << "Due:\t" << cookie.expirationDate() << "\n";
+    }
+
+    return 0;
 }
